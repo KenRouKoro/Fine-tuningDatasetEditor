950c3affb1e2a49c9fdc98209a42a62d45bf0025
koro <koro@korostudio.cn>
2024-10-04 06:12:35 +0800

    PWA Support

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -23,6 +24,24 @@ export default defineConfig({
     }),
     Components({
       resolvers: [NaiveUiResolver()]
-    })
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions:{enabled:true},
+      includeAssets:['icon.svg'],
+      manifest:{
+        name:'Fine-tuning Dataset Editor',
+        short_name:'Fine-tuningDatasetEditor',
+        description: 'My Awesome App description',
+        theme_color: '#ffc471',
+        icons: [
+          {
+            src: 'icon.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml',
+          },
+        ],
+      }
+    }),
   ]
 })
